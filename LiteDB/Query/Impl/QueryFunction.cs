dd97046d913cd91362a6663237dd623ddabec26c
Chris Szurgot <chris@szurgot.net>
2016-09-15 14:25:56 -0700

    Creates an internal query function that allows executing a function on all index values.
    Will only be exposed by an external library marked as experimental

---
+++ LiteDB/Query/Impl/QueryFunction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LiteDB
+{
+    /// <summary>
+    /// Do a full scan over the index, executing a function on each value.
+    /// Should be much faster than a full table scan.
+    /// 
+    /// Will be exposed in LiteDB.Extended package only.
+    /// </summary>
+    internal class QueryIndexWithFunction : Query
+    {
+        private Func<BsonValue, bool> func;
+
+        public QueryIndexWithFunction(string field, Func<BsonValue, bool> func)
+            : base(field)
+        {
+            this.func = func;
+        }
+
+        internal override IEnumerable<IndexNode> ExecuteIndex(IndexService indexer, CollectionIndex index)
+        {
+            return indexer
+                .FindAll(index, Query.Ascending)
+                .Where(i => func(i.Key));
+        }
+    }
+}
