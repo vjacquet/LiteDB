b3bf40b3a279557535f907a497580273d99aa998
Chris Szurgot <chris@szurgot.net>
2016-09-15 14:44:26 -0700

    Upsert functions. Rudimentary, and and marked as internal only. Will be exposed through LiteDB.Extended package

---
+++ LiteDB/Core/Collections/Upsert.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LiteDB
+{
+    public partial class LiteCollection<T>
+    {
+        /// <summary>
+        /// Upserts a document in this collection. Returns false if the document could not be inserted or updated.
+        /// </summary>
+        internal bool Upsert(T document)
+        {
+            if (document == null) throw new ArgumentNullException("document");
+
+            // get BsonDocument from object
+            var doc = _mapper.ToDocument(document);
+
+            return _engine.Upsert(_name, new BsonDocument[] { doc }) > 0;
+        }
+
+        /// <summary>
+        /// Upserts a document in this collection. Returns false if the document could not be inserted or updated.
+        /// </summary>
+        internal bool Upsert(BsonValue id, T document)
+        {
+            if (document == null) throw new ArgumentNullException("document");
+            if (id == null || id.IsNull) throw new ArgumentNullException("id");
+
+            // get BsonDocument from object
+            var doc = _mapper.ToDocument(document);
+
+            // set document _id using id parameter
+            doc["_id"] = id;
+
+            return _engine.Upsert(_name, new BsonDocument[] { doc }) > 0;
+        }
+
+        /// <summary>
+        /// Upserts all documents. Returns a count of documents inserted and updated.
+        /// </summary>
+        internal int Upsert(IEnumerable<T> documents)
+        {
+            if (documents == null) throw new ArgumentNullException("document");
+
+            return _engine.Upsert(_name, documents.Select(x => _mapper.ToDocument(x)));
+        }
+    }
+}
---
+++ LiteDB/DbEngine/DbEngine/Upsert.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LiteDB
+{
+   public partial class DbEngine
+    {
+        /// <summary>
+        /// Implement upsert command to documents in a collection. Calls update on all documents,
+        /// then any documents not updated are then attempted to insert.
+        /// This will have the side effect of throwing if duplicate items are attempted to be inserted.
+        /// </summary>
+        /// <param name="colName"></param>
+        /// <param name="documents"></param>
+        /// <returns></returns>
+        internal int Upsert(String colName, IEnumerable<BsonDocument> documents)
+        {
+            using (var trans = BeginTrans())
+            {
+                foreach (var doc in documents)
+                {
+                    var arr = new BsonDocument[] { doc };
+                    if (Update(colName, arr) == 0)
+                    {
+                        Insert(colName, arr);
+                    }
+                }
+
+                return documents.Count();
+            }
+        }
+    }
+}
