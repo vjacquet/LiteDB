b56ebbf4c4c5bb701f00e9c92201bcbc65bb627f
Chris Szurgot <chris@szurgot.net>
2016-09-15 17:08:40 -0700

    LiteDB.Extended package

---
+++ LiteDB.Extended/LiteCollectionEx.cs
@@ -0,0 +1,36 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteDB.Extended
+{
+    public static class LiteCollectionEx
+    {
+        /// <summary>
+        /// Update a document in this collection. Returns false if not found document in collection
+        /// </summary>
+        public static bool Upsert<T>(this LiteCollection<T> coll, T document) where T : new()
+        {
+            return coll.Upsert(document);
+        }
+
+        /// <summary>
+        /// Update a document in this collection. Returns false if not found document in collection
+        /// </summary>
+        public static bool Upsert<T>(this LiteCollection<T> coll, BsonValue id, T document) where T : new()
+        {
+            return coll.Upsert(id, document);
+        }
+
+        /// <summary>
+        /// Update all documents
+        /// </summary>
+        public static int Upsert<T>(this LiteCollection<T> coll, IEnumerable<T> documents) where T : new()
+        {
+            return coll.Upsert(documents);
+        }
+    }
+}
---
+++ LiteDB.Extended/QueryEx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteDB.Extended
+{
+    public class QueryEx
+    {
+        /// <summary>
+        /// Returns documents where the specified function returns true for the specified index field
+        /// </summary>
+        public static Query QueryIndexWithFunc(string field, Func<BsonValue, bool> func)
+        {
+            return new QueryIndexWithFunction(field, func);
+        }
+    }
+}
